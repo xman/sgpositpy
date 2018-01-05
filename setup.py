f5c466a84e2654dd5498d9c79a365e15f4703fce
ShinYee <shinyee@speedgocomputing.com>
2018-01-06 02:49:29 +0800

    sgposit: Basic setup for distribution.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sgposit',
+    version='0.0.1.dev1',
+    description='Posit arithmetic library for python',
+    keywords='posit arithmetic',
+    author='SpeedGo Computing',
+    author_email='shinyee@speedgocomputing.com',
+    url='https://github.com/xman/sgpositpy',
+    license='MIT',
+    install_requires=[],
+    extras_require={
+        'dev' : [],
+        'test': [],
+    },
+    packages=find_packages(exclude=['build', 'docs', 'samples', 'tests']),
+)
