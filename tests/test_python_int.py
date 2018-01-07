d95667ef0d38762d7fda893435adf790e0803a05
ShinYee <shinyee@speedgocomputing.com>
2018-01-07 13:46:53 +0800

    Test|int: Notes for operations and behaviors required on the big integer.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

---
+++ tests/test_python_int.py
@@ -0,0 +1,9 @@
+# Basic arithmetic __add__, __sub__, __mul__, __floordiv__
+# Dynamic large number.
+# Access to binary bits: bits for positive numbers and bits for 2's complement negative numbers.
+# Signed extended shift operations: <<, >>
+# Bit-wise operations: &, |, ^, ~
+
+# Operations and behaviors that are not supported shall be complemented with other functions.
+
+# Pass by value.
