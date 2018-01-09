b1ccfe2bcc98ae12a20b1f193f8fc72fc6522e20
ShinYee <shinyee@speedgocomputing.com>
2018-01-09 15:45:19 +0800

    Test|coder: Posit binary encoding and decoding symmetry test.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

--- tests/test_coder.py
+++ tests/test_coder.py
@@ -138,5 +138,14 @@ class TestCoder(unittest.TestCase):
         self.assertEqual(repstr, '0')
 
 
+    def test_encoding_decoding_symmetry_n6es2(self):
+        nbits = 6
+        es = 2
+        for bits in range(2**nbits):
+            rep = decode_posit_binary(bits, nbits, es)
+            encoded_bits = encode_posit_binary(rep)
+            self.assertEqual(encoded_bits, bits)
+
+
 if __name__ == '__main__':
     unittest.main()
