c25d4e265a5bdefa6e128c87b4c9c181cbcec673
ShinYee <shinyee@speedgocomputing.com>
2018-01-10 23:09:00 +0800

    samples: PCPosit sample arithmetic.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

---
+++ samples/pcposit_sample.py
@@ -0,0 +1,46 @@
+# MIT License
+#
+# Copyright (c) 2018 SpeedGo Computing
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+# Run: PYTHONPATH=../src python pcposit_sample.py
+
+
+from sgposit.pcposit import PCPosit
+
+
+a = PCPosit(0x0C, mode='bits', nbits=6, es=2)
+b = PCPosit(0x0F, mode='bits', nbits=6, es=2)
+
+c = a + b
+print("{} + {} => {}".format(a, b, c))
+
+d = a - b
+print("{} - {} => {}".format(a, b, d))
+
+e = a * b
+print("{} * {} => {}".format(a, b, e))
+
+f = a / b
+print("{} + {} => {}".format(a, b, f))
+
+g = -a
+print("uminus {} => {}".format(a, g))
